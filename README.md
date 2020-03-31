7c4d97eb39ca523825bc6686f9fc65c8c9a93513
Florent TONNEAU <tonneau.florent@gmail.com>
2020-03-31 15:34:02 +0200

    Gradle init

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-#### Minesweeper implementation in Kotlin
+### Minesweeper implementation in Kotlin
 
 This little project comes from the Hyperskill/Jetbrains academy. The goal here was to learn some Kotlin.
 
 NB: Tests are provided by the platform via IntelliJ EduTools plugin.
 
 Reference: [project on hyperskill website](https://hyperskill.org/projects/8)
+
+#### Run
+`./gradlew run --console=plain`
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.3.70"
+
+    // Apply the application plugin to add support for building a CLI application.
+    application
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClassName = "minesweeper.GameKt"
+
+}
+
+// Accept standard inputs in run task
+val run by tasks.getting(JavaExec::class) {
+    standardInput = System.`in`
+}
